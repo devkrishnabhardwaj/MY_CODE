8013827f69bf94ad89fe438fb2f38901d7b3acc0
DevKrishna Bhardwaj <dev.k.bhardwaj@hotmail.com>
2023-08-17 20:48:00 +0530

    4

---
+++ python/printFormat.py
@@ -0,0 +1,13 @@
+print(1,2,3,4,5,sep='-')
+print(1,2,3,4,5,sep=' ')
+
+# printing without changing line
+
+print("Hello",end=' ')
+print("World")
+
+# using print like printf in C
+
+print("a = %d b = %.2f"%(1,1.1))
+s = "string"
+print("%s"%(s))
