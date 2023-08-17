56fd13accd4bfed9408f2be865f4907fd94bad09
DevKrishna Bhardwaj <dev.k.bhardwaj@hotmail.com>
2023-08-17 20:18:21 +0530

    2

--- python/forLoop.py
+++ python/forLoop.py
@@ -2,3 +2,5 @@ for i in range(1,5):
     print(i)
 for i in "hello":
     print(i)
+    
+#
---
+++ python/format.py
@@ -0,0 +1,15 @@
+# float formatting
+a=1.11
+b=0.01
+c = format(a*b,'.2f')
+print(c)
+
+# exponential formatting
+
+print(format(123456789,'.3e'))
+
+# string formatting
+
+print(format("Hello MF",'<11'))
+print(format("Hello MF",'>11'))
+print(format("Hello MF",'^11'))
